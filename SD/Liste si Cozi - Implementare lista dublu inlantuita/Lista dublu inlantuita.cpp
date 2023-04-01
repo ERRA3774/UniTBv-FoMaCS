a0766e93671e162b5039f9a7aedc075624b0a08a
Gabriel Luican <93817419+ERRA3774@users.noreply.github.com>
2023-04-01 19:32:27 +0300

    Update Lista dublu inlantuita.cpp

--- SD/Liste si Cozi - Implementare lista dublu inlantuita/Lista dublu inlantuita.cpp
+++ SD/Liste si Cozi - Implementare lista dublu inlantuita/Lista dublu inlantuita.cpp
@@ -1,3 +1,34 @@
+// Problema 1
+// Implementarea unei liste dublu inlantuite.
+// Sa se implementeze o lista dublu inlantuita cu functionalitatile descris in continuare.
+// Se cere utilizarea unei structuri Node care are trei campuri:
+//	1. un camp pentru informatie -> int
+//  2. si doua campuri de tip pointer la Node pentru legaturile catre elementele precedent si urmator
+// Se cere utilizarea unei structuri List care are ca mebri:
+//	1. doua variable de tip Node* reprezentand primul respectiv ultimul element din lista
+//	2. o variabila de tip int reprezentand numarul de elemente din lista si functiile:
+//		a. push_front(key) - adauga cheia key in capul listei (.2p)
+//		b. push_back(key) - adauga cheia key la finalul listei (.2p)
+//		c. pop_front() - sterge primul element din lista (.2p)
+//		d. pop_back() - sterge ultimul element din lista (.2p)
+//		e. find(key) - cauta o cheie key lista - returneaza pointer la nodul cu cheia key sau NULL (.2p)
+//		f. erase(Node* node) - sterge un element Node din lista (FARA cautare). 
+//		   Node-ul trimis ca parametru a fost in prealabil gasit cu find sau identificat prin parcurgerea listei (.2p)
+//		g. remove(key) - sterge toate aparitiile cheii key (CU cautare) (.2p) 
+//		h. insert(Node* node, int val) - insereaza un element cu cheia val inainte de nodul indicat de Node. (.5p)
+//		i. empty() - verifica daca lista e vida (.2p)
+//		j. clear() - goleste lista. (.2p)
+//		k. print() - afiseaza elementele listei (.2p)
+//		l. size() - returneaza numarul de elemente din lista. 
+// De asemenea, sa se implementeze urmatoarele functii, care nu fac parte din structura:
+//	1. palindrom (List L) - verifica daca lista este palindrom (.5p)
+//	2. compare (List L1, List L2) - returneaza 1 daca L1 si L2 sunt identice si 0 altfel (.2p)
+// In functia main realizati un meniu cu ajutorul unei instructiuni switch, 
+// prin care se ofera optiuni corespunzatoare fiecarei functionalitati,
+// precum si o optiune de EXIT.
+// Intr-o instruciune while, se citesc si se executa optiuni pana la alegerea optiunea EXIT.
+// ATENTIE: Nici o functie nu trebuie sa dea eroare de exectuie, daca se apeleaza pe o lista vida.
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -459,7 +490,7 @@ int main()
 			std::cout << "Pop Back" << std::endl;
 			do {
 				std::cout << "pop_back()> ";
-				std::cout << "Popped " << list.tail << std::endl;
+				std::cout << "Popped " << list.tail->data << std::endl;
 				list.pop_back();
 				list.print();
 				std::cout << "Press ESC now to return." << std::endl;
@@ -476,7 +507,7 @@ int main()
 			std::cout << "pop_front()> ";
 			do {
 				std::cout << "pop_back()> ";
-				std::cout << "Popped " << list.head << std::endl;
+				std::cout << "Popped " << list.head->data << std::endl;
 				list.pop_front();
 				list.print();
 				std::cout << "Press ESC now to return." << std::endl;
@@ -492,7 +523,7 @@ int main()
 			std::cout << "find()> ";
 			std::getline(std::cin, input);
 			std::istringstream(input) >> data;
-			std::cout << list.find(data);
+			std::cout << list.find(data)->data;
 			_getch();
 			break;
 
@@ -503,6 +534,7 @@ int main()
 			std::getline(std::cin, input);
 			std::istringstream(input) >> data;
 			list.erase(list.find(data));
+			std::cout << "Erased " << data;
 			_getch();
 			break;
 
