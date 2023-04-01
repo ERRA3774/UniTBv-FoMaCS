225403fd8add2ab6153c77776966b041efac09e5
Gabriel Luican <93817419+ERRA3774@users.noreply.github.com>
2023-04-01 19:48:14 +0300

    Update StructuriDeDate - Problema1.cpp

--- SD/StructuriDeDate - Problema1.cpp
+++ SD/StructuriDeDate - Problema1.cpp
@@ -1,45 +1,31 @@
 // Problema 1
 // Numim perechea (x,y) pereche ordonata daca x < y
 // a) Sa se verifice daca un vector contine doar perechi ordonate (care satisfac conditia de mai sus. (0.5p)
-// !!! vec = {1,2,3,4}, se verifica {1,2}, {2,3}, {3,4}
 // b) Sa se verifice daca oricare x din prima jumatate a vectorului formeaza o pereche ordonata cu oricare y din cea de-a doua jumatate. (1p)
 
 #include <iostream>
 #include <vector>
-#define LOG(x, y) std::cout <<"[D] " << x << " " << y << std::endl;
+#include <algorithm>
 
 void problema1A();
 bool contineDoarPerechiOrdonate(std::vector<int>& vec);
 void problema1B();
 bool formeazaPerecheOrdonata(std::vector<int>& vec);
+void tests();
+#define ASSERT(x,y) (x == y) ? std::cout << "Test Passed!" << std::endl : std::cout << "Test Failed!"<< std::endl;
 
 int main()
 {
-	problema1A();
-	problema1B();
+	tests();
 }
 
 void problema1A()
 {
-	std::vector<int> vec{ 0, 1, 4, 5, 8, 9, 12, 13, 16, 19 };
+	std::vector<int> vec{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 	contineDoarPerechiOrdonate(vec)
 		? std::cout << "Contine doar perechi ordonate." << std::endl
 		: std::cout << "NU contine doar perechi ordonate." << std::endl;
-}
-
-bool contineDoarPerechiOrdonate(std::vector<int>& vec)
-{
-	for (int i = 0; i < vec.size() - 1; i++)
-	{
-		LOG(vec[i], vec[i + 1]);
-		if (vec[i] >= vec[i + 1])
-		{
-			return false;
-		}
-	}
-
-	return true;
 }
 
 void problema1B()
@@ -51,15 +37,37 @@ void problema1B()
 		: std::cout << "Oricare x si y NU formeaza perechi ordonate." << std::endl;
 }
 
+bool contineDoarPerechiOrdonate(std::vector<int>& vec)
+{
+	if (vec.size() == 0)
+	{
+		return false;
+	}
+
+	for (int i = 0; i < vec.size() - 1; i++)
+	{
+		if (vec[i] >= vec[i + 1])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
 bool formeazaPerecheOrdonata(std::vector<int>& vec)
 {
+	if (vec.size() == 0)
+	{
+		return false;
+	}
+
 	int mij = vec.size() / 2;
 
 	for (int i = 0; i < mij; i++)
 	{
 		for (int j = mij; j < vec.size(); j++)
 		{
-			LOG(vec[i], vec[j]);
 			if (vec[i] >= vec[j])
 			{
 				return false;
@@ -68,4 +76,27 @@ bool formeazaPerecheOrdonata(std::vector<int>& vec)
 	}
 
 	return true;
+}
+
+void tests()
+{
+	std::vector<int> vec0{ 1,2,3,4,5,6,7,8,9 };
+	ASSERT(formeazaPerecheOrdonata(vec0), true);
+	ASSERT(contineDoarPerechiOrdonate(vec0), true);
+
+	std::vector<int> vec1{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 4 };
+	ASSERT(formeazaPerecheOrdonata(vec1), false);
+	ASSERT(contineDoarPerechiOrdonate(vec1), false);
+
+	std::vector<int> vec2{ 9,8,7,6,5,4,1,2,3 };
+	ASSERT(formeazaPerecheOrdonata(vec2), false);
+	ASSERT(contineDoarPerechiOrdonate(vec2), false);
+
+	std::vector<int> vec3{ 1,1,1,1,1,1,1,1};
+	ASSERT(formeazaPerecheOrdonata(vec3), false);
+	ASSERT(formeazaPerecheOrdonata(vec3), false);
+
+	std::vector<int> vec4;
+	ASSERT(formeazaPerecheOrdonata(vec4), false);
+	ASSERT(formeazaPerecheOrdonata(vec4), false);
 }
